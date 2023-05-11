db4b1fd63e9fde7001ccbdeddcdd5446f0a270ff
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2023-05-11 15:47:38 +0100

    added visualisation

--- src/algorithms.py
+++ src/algorithms.py
@@ -2,7 +2,57 @@ import numpy as np
 
 class Algorithms(object):
 
-    def __init__(self):
+    # In lattice we let : I ==> 0 and A ==> 1
+    def __init__(self, l, p):
 
-        pass
+        self.l= l
+        self.p= p
+        self.upIndex= np.roll(np.indices((self.L, self.L)), -1, axis=0)
+        self.downIndex= np.roll(np.indices((self.L, self.L)), +1, axis=0)
+        self.rightIndex= np.roll(np.indices((self.L, self.L)), +1, axis=1)
+        self.leftIndex= np.roll(np.indices((self.L, self.L)), -1, axis=1)
+
+    def generateRandomIndex(self):
+
+        self.i= tuple(np.random.randint(low= 0, high= self.l, size=(self.l, self.l)))
     
+    def chooseRandomNeighbour(self):
+        # Pick random index for nearest neighbour
+        # 0 ==> up
+        # 1 ==> down
+        # 2 ==> right
+        # 3 ==> left
+        self.randomNeighbourListIndex= np.random.choice([0,1,2,3])
+    
+    def findNearestNeighbours(self):
+
+        self.up= np.roll(self.arr, -1, axis=0)
+        self.down= np.roll(self.arr, +1, axis=0)
+        self.right= np.roll(self.arr, +1, axis=1)
+        self.left= np.roll(self.arr, -1, axis=1)
+
+        self.neighbourList= [self.up[self.i], self.down[self.i], self.left[self.i], self.right[self.i]]
+        self.neighbourIndex= [self.upIndex[self.i], self.downIndex[self.i], self.leftIndex[self.i], self.rightIndex[self.i]]
+
+
+    def updateSite(self):
+
+        if self.arr[self.i]== 0:
+            pass
+        if self.arr[self.i]== 1:
+            self.chooseRandomNeighbour()
+            if np.random.rand <= 1-self.p:
+                self.arr[self.i]==0
+            if np.random.rand <= self.p & self.neighbourList[self.randomNeighbourListIndex] == 0:
+                randomNeighbourIndex= self.neighbourIndex[self.randomNeighbourListIndex]
+                self.arr[randomNeighbourIndex] == 1            
+
+    def updateLattice(self, arr):
+        
+        self.arr= arr
+        self.generateRandomIndex()
+        self.findNearestNeighbours()
+        self.updateSite()
+
+        return self.arr
+
--- src/simulation.py
+++ src/simulation.py
@@ -3,9 +3,38 @@ from animate import Animate
 from observables import Observables
 from algorithms import Algorithms
 
-class Simulation(object):
+class Simulate(object):
 
-    def __init__(self):
+    def __init__(self, l, p):
 
         self.algorithms= Algorithms()
         self.observables= Observables()
+        self.l= l
+        self.p= p
+        self.N= self.l**2
+        self.algorithms= Algorithms(self.l, self.p)
+
+    def gneerateInitialLattice(self):
+
+        self.arr= np.random.choice(np.array([0, 1, 2]), size=(self.l, self.l))
+
+    def runSimulationVisualisation(self):
+        
+        self.epoch=0
+        self.sweeps=0
+        self.animation= Animate(self.arr)
+
+        while True:
+            self.arr= self.algorithms.updateLattice(self.arr)
+            self.epoch += 1
+            
+            if self.epoch % self.N ==0:
+                self.sweeps+=1
+                print(self.sweeps)
+                self.animation.drawImage(self.arr)
+
+if __name__ == '__main__':
+    
+    # Visualisation
+    simulation= Simulate(50, 0.6)
+    simulation.runSimulationVisualisation()
